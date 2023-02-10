8c637fadee45aa79ebd446de433073fcc9c1d39b
Praneeth Jadcherla <praneethjadcherla96@gmail.com>
2023-02-10 02:11:17 +0000

    Added parameterizedTest and made final changes

--- src/main/java/com/techreturners/TDDExercises/RomanNumerals.java
+++ src/main/java/com/techreturners/TDDExercises/RomanNumerals.java
@@ -1,52 +1,58 @@
 package com.techreturners.TDDExercises;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RomanNumerals {
-    public String convertNumberToRomanNumeral(int number) {
 
-        if(number<=0 || number>3000){
+    public Map romanNumeralsMap() {
+        Map<String, Integer> romansAndNumbersMap = new LinkedHashMap<>();
+        romansAndNumbersMap.put("M", 1000);
+        romansAndNumbersMap.put("CM", 900);
+        romansAndNumbersMap.put("D", 500);
+        romansAndNumbersMap.put("CD", 400);
+        romansAndNumbersMap.put("C", 100);
+        romansAndNumbersMap.put("XC", 90);
+        romansAndNumbersMap.put("L", 50);
+        romansAndNumbersMap.put("XL", 40);
+        romansAndNumbersMap.put("X", 10);
+        romansAndNumbersMap.put("IX", 9);
+        romansAndNumbersMap.put("V", 5);
+        romansAndNumbersMap.put("IV", 4);
+        romansAndNumbersMap.put("I", 1);
+        return romansAndNumbersMap;
+    }
+
+    public String convertNumberToRomanNumeral(int number) {
+        if (number <= 0 || number > 3000) {
             return "Number cannot be converted to Roman Numeral";
         }
-        String[] romanNumbers={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        int[] integers={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        StringBuilder romanNumeral=new StringBuilder();
-
-        for(int roman=0;roman< integers.length;roman++){
-              while(number>=integers[roman]) {
-                  romanNumeral.append(romanNumbers[roman]);
-                  number=number-integers[roman];
-              }
+        Map<String, Integer> map = romanNumeralsMap();
+        StringBuilder romanNumeral = new StringBuilder();
+        for (String roman : map.keySet()) {
+            System.out.println(roman);
+            while (number >= map.get(roman)) {
+                romanNumeral.append(roman);
+                number = number - map.get(roman);
+            }
         }
         return romanNumeral.toString();
     }
 
-    public int convertRomanNumeralToNumber(String romanNumeral){
-
-
-        Map<Character, Integer> romansAndNumbers = new HashMap<>();
-        romansAndNumbers.put('M', 1000);
-        romansAndNumbers.put('D', 500);
-        romansAndNumbers.put('C', 100);
-        romansAndNumbers.put('L', 50);
-        romansAndNumbers.put('X', 10);
-        romansAndNumbers.put('V', 5);
-        romansAndNumbers.put('I', 1);
-
-        if(romanNumeral==null){
-          throw new IllegalStateException("Please enter valid Roman Numeral");
+    public int convertRomanNumeralToNumber(String romanNumeral) {
+        Map<String, Integer> map = romanNumeralsMap();
+        if (romanNumeral.equals(null)) {
+            throw new IllegalStateException("Please enter valid Roman Numeral");
         }
-
         int number = 0;
         for (int i = 0; i < romanNumeral.length(); i++) {
-            if (i+1 == romanNumeral.length() || romansAndNumbers.get(romanNumeral.charAt(i)) >= romansAndNumbers.get(romanNumeral.charAt(i + 1))) {
-                number = number + romansAndNumbers.get(romanNumeral.charAt(i));
+            if (i + 1 == romanNumeral.length() || map.get(romanNumeral.substring(i, i + 1)) >= map.get(romanNumeral.substring(i + 1, i + 2))) {
+                number = number + map.get(romanNumeral.substring(i, i + 1));
             } else {
-                number = number - romansAndNumbers.get(romanNumeral.charAt(i));
+                number = number - map.get(romanNumeral.substring(i, i + 1));
             }
         }
         return number;
     }
-
 }
--- src/test/java/com/techreturners/TDDExercises/RomanNumeralsTest.java
+++ src/test/java/com/techreturners/TDDExercises/RomanNumeralsTest.java
@@ -9,115 +9,96 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanNumeralsTest {
 
     @Test
-    public void checkNumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkNumberToRomanNumeral() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("I",romanNumerals.convertNumberToRomanNumeral(1));
+        assertEquals("I", romanNumerals.convertNumberToRomanNumeral(1));
     }
 
     @Test
-    public void checkDifferentNumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkDifferentNumberToRomanNumeral() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("III",romanNumerals.convertNumberToRomanNumeral(3));
+        assertEquals("III", romanNumerals.convertNumberToRomanNumeral(3));
     }
 
     @Test
-    public void checkNumber4ToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkNumber4ToRomanNumeral() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("IV",romanNumerals.convertNumberToRomanNumeral(4));
+        assertEquals("IV", romanNumerals.convertNumberToRomanNumeral(4));
     }
 
     @Test
-    public void checkNumber10ToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkNumber40ToRomanNumeral() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("X",romanNumerals.convertNumberToRomanNumeral(10));
+        assertEquals("XL", romanNumerals.convertNumberToRomanNumeral(40));
     }
 
     @Test
-    public void checkNumber40ToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkInvalidNumberToRomanNumeral() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("XL",romanNumerals.convertNumberToRomanNumeral(40));
+        assertEquals("Number cannot be converted to Roman Numeral", romanNumerals.convertNumberToRomanNumeral(3001));
     }
 
     @Test
-    public void checkHighNumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkInvalid0NumberToRomanNumeral() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("CCLII",romanNumerals.convertNumberToRomanNumeral(252));
+        assertEquals("Number cannot be converted to Roman Numeral", romanNumerals.convertNumberToRomanNumeral(0));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    public void checkForMultipleNumbersConvertToRomanNumerals(String romanNumeral, int number) {
+        RomanNumerals romanNumerals = new RomanNumerals();
+
+        assertEquals(romanNumeral, romanNumerals.convertNumberToRomanNumeral(number));
     }
 
     @Test
-    public void checkHigherNumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkRomanNumeralToNumberConversion() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("MXC",romanNumerals.convertNumberToRomanNumeral(1090));
+        assertEquals(1, romanNumerals.convertRomanNumeralToNumber("I"));
     }
 
     @Test
-    public void check3000NumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkVToNumberConversion() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("MMM",romanNumerals.convertNumberToRomanNumeral(3000));
+        assertEquals(5, romanNumerals.convertRomanNumeralToNumber("V"));
     }
 
     @Test
-    public void checkInvalidNumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkNinetyNineToNumberConversion() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("Number cannot be converted to Roman Numeral",romanNumerals.convertNumberToRomanNumeral(3001));
+        assertEquals(99, romanNumerals.convertRomanNumeralToNumber("IC"));
     }
 
     @Test
-    public void checkInvalid0NumberToRomanNumeral(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkRoman490ToNumberConversion() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals("Number cannot be converted to Roman Numeral",romanNumerals.convertNumberToRomanNumeral(0));
+        assertEquals(490, romanNumerals.convertRomanNumeralToNumber("XD"));
     }
 
     @Test
-    public void checkRomanNumeralToNumberConversion(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    public void checkInvalidRomanToNumberConversion() {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals(1,romanNumerals.convertRomanNumeralToNumber("I"));
+        assertEquals(0, romanNumerals.convertRomanNumeralToNumber(""));
     }
 
-    @Test
-    public void checkVToNumberConversion(){
-        RomanNumerals romanNumerals=new RomanNumerals();
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    public void checkMultipleRomanToNumberConversion(String romanNumeral, int number) {
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        assertEquals(5,romanNumerals.convertRomanNumeralToNumber("V"));
-    }
-
-    @Test
-    public void checkNinetyNineToNumberConversion(){
-        RomanNumerals romanNumerals=new RomanNumerals();
-
-        assertEquals(99,romanNumerals.convertRomanNumeralToNumber("IC"));
-    }
-
-    @Test
-    public void checkRoman490ToNumberConversion(){
-        RomanNumerals romanNumerals=new RomanNumerals();
-
-        assertEquals(490,romanNumerals.convertRomanNumeralToNumber("XD"));
-    }
-
-    @Test
-    public void checkInvalidRomanToNumberConversion(){
-        RomanNumerals romanNumerals=new RomanNumerals();
-
-        assertEquals(0,romanNumerals.convertRomanNumeralToNumber(""));
-    }
-
-    @Test
-    public void checkHigherRomanToNumberConversion(){
-        RomanNumerals romanNumerals=new RomanNumerals();
-
-        assertEquals(2950,romanNumerals.convertRomanNumeralToNumber("LMMM"));
+        assertEquals(number, romanNumerals.convertRomanNumeralToNumber(romanNumeral));
     }
 
 }
